63bf0c6c59e645ed149c71c6ccc6ef4a85cd66f1
mikey <michal_maslanka@onet.pl>
2024-08-16 11:48:19 -0100

    rizz

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -168,7 +168,7 @@ return {
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -176,8 +176,12 @@ return {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
+        tsserver = {},
+        html = {},
+        svelte = {},
+        tailwindcss = {},
+
+        swift_mesonls = {},
 
         lua_ls = {
           -- cmd = {...},
